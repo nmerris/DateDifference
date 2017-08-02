16fbd713b804efa680bff850f5e4f4264901e67d
nmerris <nate.merris@gmail.com>
2017-08-02 09:06:44 -0400

    add basic comments

--- src/com/nmerris/Main.java
+++ src/com/nmerris/Main.java
@@ -5,6 +5,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
+// this app takes in two dates, and displays the difference in days
+// format for entry is MM/DD/YY, which is checked
+// user is not reprompted if invalid date format is entered, because this is a simple excercise
+// entered dates do not need to be in chronological order
 public class Main {
 
     public static void main(String[] args) {
